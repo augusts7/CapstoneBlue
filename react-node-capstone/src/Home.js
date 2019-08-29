d142190aabe4dba20f59a5c733382335968dc71f
Nick520 <whodat520@gmail.com>
2019-08-28 23:28:34 -0500

    changed home page element

--- react-node-capstone/src/Home.js
+++ react-node-capstone/src/Home.js
@@ -1,31 +1,11 @@
 import React, { Component } from "react";
 import './Home.css'
+import EventListElement from './EventListElement.js';
+
 class Home extends Component {
-
-  onAddEvent(event){
-    console.log("erg");
-  }
-
-  onModifyEvent(event){
-    console.log("qwer");
-  }
-
-  onDeleteEvent(event){
-    console.log("mnbvcg");
-  }
-
   render() {
     return (
-      <div className="container">
-        <div className="AddEvent btn-group-vertical" >
-            <button type="button" className="btn"
-            onClick={this.onAddEvent}>Add Event</button>
-            <button type="button" className="btn"
-            onClick={this.onModifyEvent}>Modify Event</button>
-            <button type="button" className="btn"
-            onClick={this.onDeleteEvent}>Delete Event</button>
-        </div>
-      </div>
+      <EventListElement eventDescription="ULM Football Game August 30 2019" />
     );
   }
 }
