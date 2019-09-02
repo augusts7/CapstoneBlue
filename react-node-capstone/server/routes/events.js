6a6263837f14b83044e9128869060a8b0e51f21f
Nick520 <whodat520@gmail.com>
2019-09-02 01:42:03 -0500

    when adding events it now updates DB

--- react-node-capstone/server/routes/events.js
+++ react-node-capstone/server/routes/events.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const mysql = require('mysql');
 const db = require('../db/events');
 
 const router = express.Router();
+
+var connection = mysql.createConnection({
+    host: 'event-scheduler-db.cfuzjkgst1bk.us-east-2.rds.amazonaws.com',
+    user: 'admin',
+    password: 'CapstoneBlue',
+    database: 'eventschedulerdb'
+  })
+  
+connection.connect();
 
 router.get('/', async(req, res, next) => {
     try{
@@ -12,5 +22,25 @@ router.get('/', async(req, res, next) => {
         res.sendStatus(500);
     }
 });
+
+//Post
+router.post('/', (req,res) => {
+
+    var q1 = connection.query('SELECT COUNT(*) AS count FROM Events', function (error, results, fields){
+
+        if (error) throw error;
+
+        const event = {
+            eventID: results[0].count + 1,
+            eventDetails: req.body.eventDetails
+        };
+    
+        var q2 = connection.query('INSERT INTO Events SET ?', event, function (error, results, fields) {
+            if (error) throw error;
+        });
+
+        res.send(event);
+    });
+})
 
 module.exports = router;
