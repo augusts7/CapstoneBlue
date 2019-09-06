ab8cbad70ed4863a09d779c6edc16eee63772257
Nick520 <whodat520@gmail.com>
2019-09-06 14:29:01 -0500

    added material ui

---
+++ react-node-capstone/client-md/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Events from './Events';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Events />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-node-capstone/client-md/src/Events.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import MaterialTable from 'material-table';
+
+export default function MaterialTableDemo() {
+  const [state, setState] = React.useState({
+    columns: [
+      { title: 'Event Title', field: 'eventTitle' },
+      { title: 'Event Description', field: 'eventDescription' },
+      { title: 'Start Time', field: 'startTime', type: 'date-time' },
+      { title: 'End Time', field: 'endTime', type: 'date-time'},
+    ],
+    data: [],
+  });
+
+  return (
+    <MaterialTable
+      title="Events Manager"
+      columns={state.columns}
+      data={state.data}
+      editable={{
+        onRowAdd: newData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data];
+              data.push(newData);
+              setState({ ...state, data });
+            }, 600);
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data];
+              data[data.indexOf(oldData)] = newData;
+              setState({ ...state, data });
+            }, 600);
+          }),
+        onRowDelete: oldData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data];
+              data.splice(data.indexOf(oldData), 1);
+              setState({ ...state, data });
+            }, 600);
+          }),
+      }}
+    />
+  );
+}
