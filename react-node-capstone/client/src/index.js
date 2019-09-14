d33a88a3f61cc8b47e46f60307ab46ec7bf0af4a
Nick520 <whodat520@gmail.com>
2019-09-14 01:38:05 -0500

    changed client-m to client

--- react-node-capstone/client/src/index.js
+++ react-node-capstone/client/src/index.js
@@ -1,11 +1,15 @@
-import React from "react";
-import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./components/App/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import Application from './Application/Application'
 
-render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+
+
+
+ReactDOM.render(<Application />, document.getElementById('root')); 
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
--- react-node-capstone/server/server.js
+++ react-node-capstone/server/server.js
@@ -7,48 +7,6 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use("/api/events", apiRouter);
-/**
-var connection = mysql.createConnection({
-  host: 'event-scheduler-db.cfuzjkgst1bk.us-east-2.rds.amazonaws.com',
-  user: 'admin',
-  password: 'CapstoneBlue',
-  database: 'eventschedulerdb'
-})
 
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-  if (err) throw err
-
-  console.log('The solution is: ', rows[0].solution)
-})
-
-connection.end()
-
-
-*/
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-/**
-const events = [
-  {id: 1, eventDetails: 'ULM Football Game Aug 31 2019'},
-  {id: 2, eventDetails: 'Labor Day Sept 2 2019'}
-];
-
-// create a GET route
-app.get('/events', function(req, res, next){
-  res.json(events);
-});
-
-//Post
-app.post('/events', (req,res) => {
-  const event = {
-    id: events.length +1,
-    eventDetails: req.body.eventDetails
-  };
-  events.push(event);
-  res.send(event);
-})
-
-*/
