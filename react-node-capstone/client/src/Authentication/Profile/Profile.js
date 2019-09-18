64f51c1ab4ff4f865b40cd0d7a684793c6f02d39
Nick520 <whodat520@gmail.com>
2019-09-18 01:50:11 -0500

    Update Profile.js

--- react-node-capstone/client/src/Authentication/Profile/Profile.js
+++ react-node-capstone/client/src/Authentication/Profile/Profile.js
@@ -1,36 +1,48 @@
-
-import React from 'react';
+import React from "react";
 import Table from "../../Components/Table/Table";
-
-
+import "./Profile.css";
 
 class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props);
+  render() {
+    let studentName = "Student Name";
+    let userInfoHeader = ["User Information"];
+    let userInfo = [
+      ["Email", "jdoe@warhawks.ulm.edu"],
+      ["Classification", "Sophomore"],
+      ["CWID", "12345678"],
+      ["Advisor", "Lon Smith"]
+    ];
 
-        
-    }
+    let groupsHeader = ["Current Groups"];
+    let groups = [["group1"], ["group2"], ["group2"], ["group2"]];
 
-    render() {
-
-        let data = [["Email", "sanjeeb.sangraula@gmail.com"], ["First Name", "Sanjeeb"], ["Last Name", "Sangraula"],
-        [""]];
-
-        let headers = ["Title", "Data"];
-
-        return (
-            <Table selectable={true} data={data} headers={headers}  /> 
-        );
-    }
+    return (
+      <div className="profile-page">
+        <h2 className="name">{studentName}</h2>
+        <div className="grid-container">
+          <div className="user-info">
+            <Table data={userInfo} header={userInfoHeader} />
+          </div>
+          <div className="current-groups">
+            <Table data={groups} header={groupsHeader} />
+          </div>
+          <div className="classes-taken">
+            <Table data={groups} header={groupsHeader} />
+          </div>
+          <div className="shared-with">
+            <Table data={groups} header={groupsHeader} />
+          </div>
+          <div className="shared-with-me">
+            <Table data={groups} header={groupsHeader} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Profile;
-
-
-
-
-
-
-
-
