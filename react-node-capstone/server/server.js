1c569881a3a73c60ff7c0b6835963c3d92603a7c
Nick520 <whodat520@gmail.com>
2019-09-18 01:43:07 -0500

    Update server.js

--- react-node-capstone/server/server.js
+++ react-node-capstone/server/server.js
@@ -6,13 +6,9 @@ const port = process.env.PORT || 5000; //Creates a port varible to be used later
 const eventsRouter = require("./routes/events");
 const usersRouter = require("./routes/users");
 
-<<<<<<< HEAD
 //Connects Routes to there files.
 app.use("/events", eventsRouter);
 app.use("/users", usersRouter);
-=======
-app.use("/api/events", apiRouter);
->>>>>>> master
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
