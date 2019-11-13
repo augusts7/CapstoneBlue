bd0938a7a1ed24552c8c1fdc3eddcfbb2a221552
coltonray <54559245+coltonray@users.noreply.github.com>
2019-11-13 09:54:58 -0600

    added /createGroup
    needs to be hooked up to the backend

--- react-node-capstone/client/src/components/Carousel/HomeCarousel.js
+++ react-node-capstone/client/src/components/Carousel/HomeCarousel.js
@@ -13,13 +13,17 @@ class HomeCarousel extends React.Component {
         width="1650px"
         showThumbs={false}
         showStatus = {false}
+        className="presentation-mode"
         
       >
-        <div>
+        <div className="my-slide primary">
           <img
             src="https://www.wallpaperup.com/uploads/wallpapers/2014/11/12/514433/474ec055f0eaf86e7e91637bbaa05ce6-500.jpg"
             alt=""
           />
+          <h1>
+            Test
+          </h1>
           <p className="legend">Legend</p>
         </div>
         <div>
@@ -27,21 +31,16 @@ class HomeCarousel extends React.Component {
             src="https://www.wallpaperup.com/uploads/wallpapers/2014/11/12/514433/474ec055f0eaf86e7e91637bbaa05ce6-500.jpg"
             alt=""
           />
-          <p className="legend">Legend</p>
+          <h1>
+            Test
+          </h1>
+          <p className="legend">Writing whatever I want</p>
         </div>
         <div>
           <img
             src="https://www.wallpaperup.com/uploads/wallpapers/2014/11/12/514433/474ec055f0eaf86e7e91637bbaa05ce6-500.jpg"
             alt=""
           />
-          <p className="legend">Legend</p>
-        </div>
-        <div>
-          <img
-            src="https://www.wallpaperup.com/uploads/wallpapers/2014/11/12/514433/474ec055f0eaf86e7e91637bbaa05ce6-500.jpg"
-            alt=""
-          />
-          <p className="legend">Legend</p>
         </div>
         <div>
           <img
